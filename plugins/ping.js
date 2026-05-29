fd521b60aa785289806927a772baa6e8ab78356f
ratulbot01 <mdratulislam6981@gmail.com>
2026-05-29 22:22:04 +0600

    Add ping plugin to respond with 'Pong'

---
+++ plugins/ping.js
@@ -0,0 +1,13 @@
+module.exports = {
+
+    name: "ping",
+
+    async execute(sock, msg) {
+
+        const from = msg.key.remoteJid
+
+        await sock.sendMessage(from, {
+            text: "🏓 Pong"
+        })
+    }
+}
