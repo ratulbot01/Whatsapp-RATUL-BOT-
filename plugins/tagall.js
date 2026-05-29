6a8e8d770519d3e732a7607c5fb97c90112ddd32
ratulbot01 <mdratulislam6981@gmail.com>
2026-05-29 22:32:20 +0600

    Refactor tagall command to simplify execution

--- plugins/tagall.js
+++ plugins/tagall.js
@@ -1,40 +1,13 @@
 module.exports = {
-    name: "tagall",
-    alias: ["hidetag", "all"],
 
-    async execute(sock, msg, args, config) {
+    name: "tagall",
+
+    async execute(sock, msg) {
 
         const from = msg.key.remoteJid
 
-        // Group check
-        if (!from.endsWith("@g.us")) {
-            return sock.sendMessage(from, {
-                text: "❌ This command only works in groups!"
-            })
-        }
-
-        // Get group metadata
-        const metadata = await sock.groupMetadata(from)
-
-        const participants = metadata.participants
-
-        // Message
-        let teks = `🌸 *${config.BOT_NAME} TAG ALL*\n\n`
-
-        for (let mem of participants) {
-
-            teks += `➤ @${mem.id.split("@")[0]}\n`
-        }
-
-        // Mentions
-        const mentions = participants.map(a => a.id)
-
-        // Send message
         await sock.sendMessage(from, {
-            text: teks,
-            mentions
-        }, {
-            quoted: msg
+            text: "📢 Tag All Feature Working"
         })
     }
 }
