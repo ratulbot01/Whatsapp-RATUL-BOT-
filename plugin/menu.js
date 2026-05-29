a9e4c97099d01f7d42998aa11eddfa9db2893013
ratulbot01 <mdratulislam6981@gmail.com>
2026-05-29 20:35:56 +0600

    Create menu.js

---
+++ plugin/menu.js
@@ -0,0 +1,38 @@
+const fs = require("fs")
+
+module.exports = {
+
+    name: "menu",
+
+    async execute(sock, msg, args, config) {
+
+        const from = msg.key.remoteJid
+
+        const image = fs.readFileSync("./media/owner.jpg")
+
+        const menuText = `
+╔═══〔 ${config.BOT_NAME} 〕═══╗
+
+👑 Owner : ${config.OWNER_NAME}
+📱 Number : ${config.OWNER_NUMBER}
+⚡ Version : ${config.BOT_VERSION}
+
+╭── COMMANDS ──╮
+│ .menu
+│ .ping
+│ .tagall
+│ .kickall
+│ .welcome
+│ .goodbye
+│ .antilink
+│ .song
+│ .play
+╰─────────────╯
+`
+
+        await sock.sendMessage(from, {
+            image: image,
+            caption: menuText
+        })
+    }
+}
