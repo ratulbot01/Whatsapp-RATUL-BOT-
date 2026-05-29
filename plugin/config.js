e8e425b53bf35e4047081ff65d91f143387e9e06
ratulbot01 <mdratulislam6981@gmail.com>
2026-05-29 20:37:29 +0600

    Create config.js

---
+++ plugin/config.js
@@ -0,0 +1,14 @@
+module.exports = {
+
+    BOT_NAME: "RATUL BOT",
+    OWNER_NAME: "Ratul",
+    OWNER_NUMBER: "8801305891747",
+    BOT_VERSION: "1.0.0",
+    PREFIX: ".",
+
+    AUTO_READ: true,
+    AUTO_TYPING: false,
+
+    PAIR_NUMBER: "8801305891747"
+
+}
